05a3a7b39cc1b0502b5e37b3492f7a7593dfbf9d
Ethan Mann <ethanmann@Ethans-MacBook-Air-2.local>
2018-08-05 17:29:49 -0500

    added jinja

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import webapp2
-# import jinja2
-# import os
+import jinja2
+import os
 
-# jinja_env = jinja2.Environment(
-#     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-#     extensions=['jinja2.ext.autoescape'],
-#     autoescape=True) #creates environment variable for HTML rendering
+jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True) #creates environment variable for HTML rendering
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        return self.response.write("TEST")
+        self.response.headers['Content-Type'] = 'text/html'
+        template = jinja_env.get_template('app.html')
+        return self.response.write(template.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
