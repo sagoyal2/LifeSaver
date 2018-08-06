fdb157401d39f1b71832919da07e7c6bd6381df5
ethanmann <e.milomann@gmail.com>
2018-08-05 20:26:36 -0500

    added Database.py contents and new key.py with zip key

--- Database.py
+++ Database.py
@@ -1,1 +1,8 @@
-#TEST
+from google.appengine.ext import db
+
+class Subscriber(db.Model):
+    name = db.StringProperty(required=True)
+    location = db.StringProperty(required=True)
+    birthdate = db.DateProperty()
+    weight_in_pounds = db.IntegerProperty()
+    spayed_or_neutered = db.BooleanProperty()
