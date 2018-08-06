0585f9d9af19322596dda35a68692e5e36aedcaf
ethanmann <e.milomann@gmail.com>
2018-08-05 20:30:45 -0500

    added correct fields to Database.py

--- Database.py
+++ Database.py
@@ -1,8 +1,8 @@
-from google.appengine.ext import db
+from google.appengine.ext import db #imports Google Datastore
 
-class Subscriber(db.Model):
-    name = db.StringProperty(required=True)
-    location = db.StringProperty(required=True)
-    birthdate = db.DateProperty()
-    weight_in_pounds = db.IntegerProperty()
-    spayed_or_neutered = db.BooleanProperty()
+class Subscriber(db.Model): #defines Subscriber data type
+    name = db.StringProperty(required=True) #holds user name
+    zipcode = db.StringProperty(required=True) #holds user zip
+    email = db.StringProperty(required=True) #holds user email
+    phone_number = db.StringProperty(required=True) #holds user phone number
+    phone_carrier = db.StringProperty(required=True) #holds user phone carrier
