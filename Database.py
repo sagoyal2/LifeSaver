9453e6368482b7dd4472c4c6d34d3f4ccbca18c5
ethanmann <e.milomann@gmail.com>
2018-08-08 16:24:45 -0500

    changed back to previous type based on documentation of None

--- Database.py
+++ Database.py
@@ -3,6 +3,6 @@ from google.appengine.ext import db #imports Google Datastore
 class Subscriber(db.Model): #defines Subscriber data type
     name = db.StringProperty(required=True) #holds user name
     zipcode = db.StringProperty(required=True) #holds user zip
-    email = db.StringProperty() #holds user email
-    phone_number = db.StringProperty() #holds user phone number as 10 digit string
-    phone_carrier = db.StringProperty() #holds user phone carrier
+    email = db.StringProperty(required=True) #holds user email
+    phone_number = db.StringProperty(required=True) #holds user phone number as 10 digit string
+    phone_carrier = db.StringProperty(required=True) #holds user phone carrier
