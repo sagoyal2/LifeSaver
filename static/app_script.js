b2e0b83d92b67e3b163d46ade99b08729eab8573
ethanmann <e.milomann@gmail.com>
2018-08-12 19:26:59 -0500

    added document listener and changed sequence of events to be 1. obtain location, 2. feed to HTML, 3. on button click, post

--- static/app_script.js
+++ static/app_script.js
@@ -1,31 +1,43 @@
-var latInput = document.getElementById("latitudeInput");
-var lonInput = document.getElementById("longitudeInput");
-var button = document.getElementById("reportButton");
+window.addEventListener("load", mainScript)
 
-button.addEventListener("click", onClick())
+function mainScript(){
 
-function onClick(){
+  //sets HTML variables
+  var latInput = document.getElementById("latitudeInput");
+  var lonInput = document.getElementById("longitudeInput");
+  var button = document.getElementById("reportButton");
 
-  //https://www.w3schools.com/jsref/met_win_confirm.asp
-  var r = confirm("Are you sure you want to report an incident?");
-  if (r == true) {
-    getLocation();
-  }
-}
+  //requests to get location upon page loading
+  getLocation();
 
-}
+  //adds listener for button click
+  button.addEventListener("click", onClick);
 
-//https://www.w3schools.com/htmL/html5_geolocation.asp
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        console.log("Geolocation is not supported by this browser.");
+  //runs when button is clicked
+  function onClick(){
+
+    //https://www.w3schools.com/jsref/met_win_confirm.asp
+    //chance to confirm
+    var confirmReport = confirm("Are you sure you want to report an incident?");
+    if (confirmReport == true) {
+      //calls post method
+      console.log("POST")
     }
-}
-function showPosition(position) {
-    latInput.value = position.coords.latitude;
-    lonInput.value = position.coords.longitude;
-    console.log(latInput.value);
-    console.log(lonInput.value);
+  }
+
+  //https://www.w3schools.com/htmL/html5_geolocation.asp
+  function getLocation() {
+      if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+          console.log("Geolocation is not supported by this browser.");
+      }
+  }
+
+  function showPosition(position) {
+      latInput.value = position.coords.latitude;
+      lonInput.value = position.coords.longitude;
+      console.log(latInput.value);
+      console.log(lonInput.value);
+  }
 }
