5567fb47de4b4f9b0046c742f1e5ac6432680f31
ethanmann <e.milomann@gmail.com>
2018-08-19 15:42:12 -0500

    syntax fix

--- main.py
+++ main.py
@@ -77,9 +77,9 @@ class RegisterHandler(webapp2.RequestHandler):
 
         #CONFIRMATION NOTIFICATIONS
         if len(email) > 0:
-            EmailMain2.SendOneEmail(email, "Thank you for subscribing!", "We will contact you at this email if there is ever an incident near your work or home ZIP code."):
+            EmailMain2.SendOneEmail(email, "Thank you for subscribing!", "We will contact you at this email if there is ever an incident near your work or home ZIP code.")
         if len(phone_number) > 0:
-            EmailMain2.SendOneEmail(ExtraMethods.getPhoneNumberEmail(phone_number, phone_carrier), "Thank you for subscribing!", "We will contact you at this phone number if there is ever an incident near your work or home ZIP code."):
+            EmailMain2.SendOneEmail(ExtraMethods.getPhoneNumberEmail(phone_number, phone_carrier), "Thank you for subscribing!", "We will contact you at this phone number if there is ever an incident near your work or home ZIP code.")
 
 
         data = {
